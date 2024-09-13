21625f7788d90fd99c9b5d5fc24e98386cc49e5b
rjohnsen <rogercbjohnsen@gmail.com>
2024-09-13 12:56:33 +0200

    Documented workstation installer and added requirement for Ansible

--- install-workstation.sh
+++ install-workstation.sh
@@ -1,14 +1,29 @@
 #!/bin/env bash
 
+echo "Threathuning Workstation - Installer"
+echo "By Roger C.B. Johnsen - www.predefender.com"
+echo ""
+
+# Ask for password for admin user on OpenSearch
 read -s -p "Enter new OpenSearch Admin password: " pass
 
+# Ensure Alma is fully updated
 dnf update -y
 dnf install -y epel-release
+
+# Install Ansible
 dnf install ansible -y
 
+# Install requirements for Ansible installation script
+ansible-galaxy collection install ansible.posix
+
+# Obtain installation files
 curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/setup-ansible.yml -o setup-ansible.yml
 curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/hosts.ini -o hosts.ini
 curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/docker-compose.yml -o docker-compose.yml
 
+# Set password for OpenSearch
 sed -i "s/REPLACEME/$pass/g" docker-compose.yml
+
+# Run Ansible playbook
 ansible-playbook -c hosts.ini setup-ansible.yml
