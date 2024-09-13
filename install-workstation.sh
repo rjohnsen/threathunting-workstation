f585d5450b25a5aaba4e2b9959656d5b0a08d2d1
rjohnsen <rogercbjohnsen@gmail.com>
2024-09-13 12:45:38 +0200

    Renamed Ansible Yaml file

--- install-workstation.sh
+++ install-workstation.sh
@@ -6,9 +6,9 @@ dnf update -y
 dnf install -y epel-release
 dnf install ansible -y
 
-curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/setup.yml -o setup.yml
+curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/setup-ansible.yml -o setup-ansible.yml
 curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/hosts.ini -o hosts.ini
 curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/docker-compose.yml -o docker-compose.yml
 
 sed -i "s/REPLACEME/$pass/g" docker-compose.yml
-ansible-playbook -c hosts.ini setup.yml
+ansible-playbook -c hosts.ini setup-ansible.yml
