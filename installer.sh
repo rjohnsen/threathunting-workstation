87be8573aabaf857b350e23005703b2e7b2084a1
rjohnsen <rogercbjohnsen@gmail.com>
2025-01-25 17:41:49 +0100

    Refactoring installer

--- installer.sh
+++ installer.sh
@@ -5,25 +5,29 @@ echo "By Roger C.B. Johnsen - www.predefender.com"
 echo ""
 
 # Ask for password for admin user on OpenSearch
-read -s -p "Enter new OpenSearch Admin password: " pass
+# read -s -p "Enter new OpenSearch Admin password: " pass
 
 # Ensure Alma is fully updated
 dnf update -y
 dnf install -y epel-release
 
-# Install Ansible
-dnf install ansible -y
+# Install Ansible and Git
+dnf install git ansible -y
 
 # Install requirements for Ansible installation script
 ansible-galaxy collection install ansible.posix
 
 # Obtain installation files
-curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/setup-ansible.yml -o setup-ansible.yml
-curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/hosts.ini -o hosts.ini
-curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/docker-compose.yml -o docker-compose.yml
+# curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/setup-ansible.yml -o setup-ansible.yml
+# curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/hosts.ini -o hosts.ini
+# curl https://raw.githubusercontent.com/rjohnsen/threathunting-workstation/main/docker-compose.yml -o docker-compose.yml
 
 # Set password for OpenSearch
-sed -i "s/REPLACEME/$pass/g" docker-compose.yml
+# sed -i "s/REPLACEME/$pass/g" docker-compose.yml
+
+# Get installation files
+git clone https://github.com/rjohnsen/threathunting-workstation.git threathunting-workstation
+cd threathunting-workstation/
 
 # Run Ansible playbook
-ansible-playbook -c hosts.ini setup-ansible.yml
+ansible-playbook -c hosts.ini opensearch.yml
