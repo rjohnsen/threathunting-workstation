b9c2617cd0b58be44ee66505cdae986452f97f9c
rjohnsen <rogercbjohnsen@gmail.com>
2025-02-02 10:40:48 +0100

    Trying Ubuntu route

--- installer.sh
+++ installer.sh
@@ -4,25 +4,26 @@ echo "Threathunting Workstation - Installer"
 echo "By Roger C.B. Johnsen - www.predefender.com"
 echo ""
 
-# Ensure Alma is fully updated
-dnf update -y
-dnf install -y epel-release
-
 # Install Ansible and Git
-dnf install git ansible -y
+sudo apt install git ansible -y
 
 # Install requirements for Ansible installation script
-ansible-galaxy collection install ansible.posix
+sudo ansible-galaxy collection install ansible.posix
+
+# Make room for predefender tools
+mkdir /opt/predefender
+cd /opt/predefender
 
 # Get installation files
-git clone https://github.com/rjohnsen/threathunting-workstation.git workstation
-cd workstation/
+git clone https://github.com/rjohnsen/threathunting-workstation.git threathunting-workstation
+cd threathunting-workstation/
 git checkout v2
 git pull
 
 # Set password for OpenSearch
 read -s -p "Enter new OpenSearch Admin password: " pass
-sed -i "s/REPLACEME/$pass/g" docker-compose.yml
+touch .env
+echo "OPENSEARCH_INITIAL_ADMIN_PASSWORD=$pass" | tee -a .env
 
 # Run Ansible playbook
-ansible-playbook -c hosts.ini opensearch-rpm.yml
+ansible-playbook -c hosts.ini ubuntu.yml
